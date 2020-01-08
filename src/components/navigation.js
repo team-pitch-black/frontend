69a17c1c3f5590a3e0b60e6711387a76999cdecd
Matt Herich <mjherich@gmail.com>
2020-01-08 14:29:24 -0800

    Add custom logo

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -39,9 +38,9 @@ export default function MenuAppBar({isLoggedIn, setIsLoggedIn}) {
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" className={classes.title} >
-                        <Link to="/" style={{textDecoration: "none", color: "white"}}>Pitch Black</Link>
-                    </Typography>
+                    <Link to="/" style={{textDecoration: "none", color: "white"}} className={classes.title} >
+                        <img src={`${process.env.PUBLIC_URL}/cave-192.png`} width="35" height="35" alt="Pitch Black Logo" />
+                    </Link>
                     {isLoggedIn ? (
                         <div>
                             <ButtonGroup color="secondary" variant="contained">
