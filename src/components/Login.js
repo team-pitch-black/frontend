4bf56b286b4930a0c9739717f7f2cf5864a3163c
Matt Herich <mjherich@gmail.com>
2020-01-09 08:08:33 -0800

    Add snackbar for invalid logins

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import axios from 'axios'
+import Snackbar from '@material-ui/core/Snackbar'
+import MuiAlert from '@material-ui/lab/Alert'
+import { Link } from 'react-router-dom'
 
 import FormControl from '@material-ui/core/FormControl'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 class UserLogin extends React.Component {
     state = {
         credentials: {
             username: '',
             password: ''
-        }
+        },
+        open: false
     }
+
+    // Close Snackbar
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        this.setState({
+            ...this.state,
+            open: false
+        })
+    };
 
     // onChange handler
     handleChange = e => {
@@ -33,7 +52,14 @@ class UserLogin extends React.Component {
                 this.props.setIsLoggedIn(true)
                 this.props.history.push('/')
             })
-            .catch(err => console.log(err.response))
+            .catch(err => {
+                // Display Snackbar
+                this.setState({
+                    ...this.state,
+                    open: true
+                })
+                console.log(err.response)
+            })
     }
 
     render() {
@@ -60,7 +86,12 @@ class UserLogin extends React.Component {
                                 onChange={this.handleChange}
                                 style={{ marginBottom: "10px" }}
                             />
-                            <Button variant="contained" onClick={this.login} style={{ marginBottom: "20px" }}>Log In</Button>
+                            <Button variant="contained" onClick={this.login} style={{ marginBottom: "20px" }} color="secondary" type="submit">Log In</Button>
+                            <Snackbar open={this.state.open} autoHideDuration={6000} onClose={this.handleClose}>
+                                <Alert onClose={this.handleClose} severity="success">
+                                    Username not found, try again or sign up here
+                                </Alert>
+                            </Snackbar>
                         </FormControl>
                     </form>
                 </div>
